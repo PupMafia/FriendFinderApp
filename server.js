6fb121fcf9544dda33cbbae184e32174235a5bd1
PupMafia <joshsan111@gmail.com>
2018-05-03 12:04:14 -0400

    w.i.p

--- server.js
+++ server.js
@@ -1,4 +1,47 @@
 var express = require("express");
 var bodyParser = require ("body-parser");
 var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all characters
+app.get("", function(req, res) {
+  return res.json(characters);
+});
+
+
+// Create New Characters - takes in JSON input
+app.post("", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
